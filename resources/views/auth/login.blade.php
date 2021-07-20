ea1c04f0887cae91d906857542b7f1be2656c5bc
isotop786 <marufislam77@gmail.com>
2021-07-21 00:20:27 +0600

    login/register complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,13 @@ Login
 
 <div class="row my-3">
 <div class="col-md-3">
-    @if(session('status'))
-        <div class="alert alert-danger">{{session('status')}}</div>
-    @endif
 </div>
     <div class="col-md-6 bg-light p-4 mt-4">
+
+        @if(session('status'))
+            <div class="alert alert-danger">{{session('status')}}</div>
+        @endif
+
 
             <form action="{{route('login')}}" method="post" enctype="multipart/form-data" >
             @csrf
